a4e125550878f066142940d6d09dce004a81ed7c
mahdiehmaleki96 <mahdiehmaleki96@gmail.com>
2025-04-05 02:15:23 +0000

    completed react-components

---
+++ react-components/my-app/src/Caption.tsx
@@ -0,0 +1,3 @@
+export function Caption() {
+  return <h3>10 Facts about space</h3>;
+}
---
+++ react-components/my-app/src/Image.tsx
@@ -0,0 +1,5 @@
+export function Image() {
+  return (
+    <img src="https://www.universeoptics.com/wp-content/uploads/High-Resolution_Images.jpg"></img>
+  );
+}
