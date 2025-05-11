cae587bc5c81ce39d1a8a5e13526b8c8835d5333
mahdiehmaleki96 <mahdiehmaleki96@gmail.com>
2025-05-11 19:16:21 +0000

    completed react-search

---
+++ react-search/my-app/src/App.tsx
@@ -0,0 +1,11 @@
+import { SearchableList } from './components/SearchableList';
+import './App.css';
+
+export function App() {
+  return (
+    <main>
+      <h1>Harry Potter Quote Finder</h1>
+      <SearchableList />
+    </main>
+  );
+}
---
+++ react-search/my-app/src/components/SearchableList.tsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import { SearchBar } from './SearchBar';
+import { ItemList } from './ItemList';
+
+const quotes = [
+  'Yer a wizard Harry.',
+  "I hope you're pleased with yourselves.",
+  'It does not do well to dwell on dreams and forget to live.',
+  'To the well-organized mind, death is but the next great adventure.',
+  "You're a little scary sometimes, you know that? Brilliant... but scary.",
+  'There will be no foolish wand-waving or silly incantations in this class.',
+  'It takes a great deal of bravery to stand up to our enemies, but just as much to stand up to our friends.',
+  'If there is one thing Voldemort cannot understand, it is love.',
+  'As much money and life as you could want!',
+  'The truth. It is a beautiful and terrible thing, and should therefore be treated with great caution.',
+  'There are some things you can’t share without ending up liking each other.',
+  'Ah, music. A magic beyond all we do here!',
+];
+
+export function SearchableList() {
+  const [searchText, setSearchText] = useState('');
+
+  const filteredQuotes = quotes.filter((quote) =>
+    quote.toLowerCase().includes(searchText.toLowerCase())
+  );
+
+  return (
+    <div className="searchable-list">
+      <SearchBar value={searchText} onSearchChange={setSearchText} />
+      <ItemList items={filteredQuotes} />
+    </div>
+  );
+}
